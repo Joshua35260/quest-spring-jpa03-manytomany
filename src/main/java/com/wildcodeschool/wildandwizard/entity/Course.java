2f5d47f134bc5bec10e2e6996b9dffff455094cd
Joshua Dupin <joshua35260@gmail.com>
2023-03-11 16:15:25 +0100

    ok

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -1,11 +1,13 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Course {
@@ -15,9 +17,10 @@ public class Course {
     private Long id;
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "wizard_id")
-    private Wizard wizard;
+    @ManyToMany(mappedBy = "courses")
+    private List<Wizard> wizards = new ArrayList<>();
+    
+
     public Course() {
     }
 
